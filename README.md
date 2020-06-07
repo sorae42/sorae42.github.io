4ab3925acb2b81a733d9ea43b45efa464512e98a
Owlly Quest <bonniefoxy2009@gmail.com>
2020-06-08 00:05:21 +0700

    Update main files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # skyventuree.github.com
 Base page for my GitHub Page.
+
+# Copyright
+Please do not copy/duplicate and make this repository as yours.
---
+++ tools/premise/script.js
@@ -0,0 +1,4 @@
+alert(document.getElementById("switchrole").innerHTML);
+
+if (document.getElementById("switchrole").checked) {document.getElementById("switchlore").innerHTML = "They can switch their roles!";}
+else {document.getElementById("switchlore").innerHTML = "They won't be able to switch role.";}

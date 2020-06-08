d7aadfb12239051f973c7daf3f5d2d9bc2e0b4f9
Owlly Quest <bonniefoxy2009@gmail.com>
2020-06-08 07:02:31 +0700

    h

--- tools/premise/script.js
+++
@@ -1,4 +0,0 @@
-alert(document.getElementById("switchrole").innerHTML);
-
-if (document.getElementById("switchrole").checked) {document.getElementById("switchlore").innerHTML = "They can switch their roles!";}
-else {document.getElementById("switchlore").innerHTML = "They won't be able to switch role.";}

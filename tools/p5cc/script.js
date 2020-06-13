12471fbc88d20b8c67b6aa84f7d53d2c91ec6782
Owlly Quest <bonniefoxy2009@gmail.com>
2020-06-13 12:23:21 +0700

    fix delete request

--- tools/p5cc/script.js
+++ tools/p5cc/script.js
@@ -12,17 +12,16 @@ function findGetParameter(parameterName) {
 window.onload = function () {
 document.getElementById("sharelink").value = window.location;
 document.getElementById("result").src = "https://ik.imagekit.io/p5cc/p5cc/" + findGetParameter("view");
-
 }
 
 function deleteImage() {
     document.getElementById("deleteimg").innerHTML = "Please wait...";
+    var url = "https://rouxkarlus.pythonanywhere.com/p5cc/delete";
+    var params = "fileId=" + findGetParameter("file_id");
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://rouxkarlus.pythonanywhere.com/p5cc/delete", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-            fileId: findGetParameter("file_id")
-    }));
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(params);
     window.setTimeout(function() {window.location.href = "index.html";}, 3500)
 }
 

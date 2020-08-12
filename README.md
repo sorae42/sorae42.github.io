86b8128d449f57b0ea076331bb9569b2e0f64c0d
Roux Karlus <skyventuree@users.noreply.github.com>
2020-08-12 23:28:42 +0700

    overhauled commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+# What is this?
+A repository for my website.
+You shouldn't checking out the source code because I don't know, I don't wanna spoil the movie. (Bad pun intended.)
+
 # Copyright
 All of the content in this repository/website are prohibited from copying/duplicating for commercials/personal (with outcome) use. No license is used on this website.
--- tools/p5cc/script/status.js
+++
@@ -1,4 +0,0 @@
-var imgNo = 0;
-function checkServerStatus(e,n){var t=document.body.appendChild(document.createElement("img"));t.hidden=!0,t.id=imgNo,t.onload=function(){document.getElementById(e).innerHTML="online",document.getElementById(e).style.color="green"},t.onerror=function(){document.getElementById(e).innerHTML="offline",document.getElementById(e).style.color="red"},t.src=n,imgNo=+1}
-checkServerStatus("sv1", "https://rouxkarlus.pythonanywhere.com/p5cc/online.png");
-checkServerStatus("sv2", "https://p5cc.pythonanywhere.com/p5cc/online.png");

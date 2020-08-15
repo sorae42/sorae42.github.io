4ccc32fa0bffa846fbcdabaf990be97f2c652c02
Roux Karlus <skyventuree@users.noreply.github.com>
2020-08-15 15:31:16 +0700

    removed direct sharing because of error

--- tools/imganon/script.js
+++ tools/imganon/script.js
@@ -1,16 +1,7 @@
 $(document).ready(
 function() {$('.faq_question').click(function() {if ($(this).parent().is('.open')){$(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);$(this).closest('.faq').removeClass('open');}else{var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';$(this).closest('.faq').find('.faq_answer_container').animate({'height':newHeight},500);$(this).closest('.faq').addClass('open');}});
-
-});
-
 $('#protected').click(function() {
     $("#password").toggle(this.checked);
 });
+}); 
 
-function prevent() {
-    $('#txtw').text('Please wait!...');
-}
-
-function viewimg() {
-    window.location.replace($("https://rouxkarlus.pythonanywhere.com/imganon/view/" + $('#shareid').val())
-}

7acb0d189a141d75022b026a3c2fe0cb41b6a2f3
Rosetta Emerson / こじめ <skyventuree@users.noreply.github.com>
2021-01-01 00:27:29 +0700

    remove p5cc pa

--- tools/p5cc/script/index.js
+++
@@ -1,6 +0,0 @@
-const serverList = ['https://rouxkarlus.pythonanywhere.com/p5cc', 'https://p5cc.pythonanywhere.com/p5cc']
-function sendServer() {
-    var s = $("#server").val();
-    $("#requestcard").attr('action', serverList[s]);
-}
-

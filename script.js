9b3ab7a2ba7dd3fa0453c22d4147ec648d189241
sora <34794115+skyventuree@users.noreply.github.com>
2022-05-26 23:39:20 +0700

    remove unnecessary

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
 let currentYear = new Date().getFullYear();
 
 document.querySelector('#currentyear').innerHTML = currentYear;
-
-// name changing
-const names = ['Rosetta', 'skyventuree', 'sorae_42', 'shigukk', 'jdewfiez']
-const displayName = document.querySelector('#name');
-let currentName = 1;
-setInterval(() => {
-    displayName.classList.add('hidden');
-    setTimeout(() => {
-        displayName.innerHTML = names[currentName];
-        displayName.classList.remove('hidden');
-        currentName === names.length - 1 ? currentName = 0 : currentName++;
-    }, 500);
-}, 3500);

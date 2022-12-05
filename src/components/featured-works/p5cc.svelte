d6acf919850684b5a7ac6a44865ca49be328ac97
sora <34794115+sorae42@users.noreply.github.com>
2022-12-06 01:16:15 +0700

    new work entry

---
+++ src/components/featured-works/p5cc.svelte
@@ -0,0 +1,94 @@
+<div id="p5cc">
+    <img src="../../assets/p5cc-card.jpeg" alt="P5CC card sample" />
+    <div class="content">
+        <h3>~ Featured Project ~</h3>
+        <h1>Persona 5 Calling Card Maker</h1>
+        <p>
+            For the sake of effortlessly making a calling card.
+            <br />Sending your card to the darkest desire.
+        </p>
+        <span class="link">
+            <a href="https://skyventuree.github.io/p5cc"
+                ><button>Visit site<i class="fa fa-external-link" /></button></a
+            >
+            <a href="https://github.com/skyventuree/p5cc"
+                ><button><i class="fa fa-github" />Source code</button></a
+            >
+        </span>
+    </div>
+</div>
+
+<style lang="scss">
+    h1,
+    p {
+        margin-left: 0;
+        margin-top: 0;
+    }
+
+    div#p5cc{
+        display: flex;
+        flex-direction: row-reverse;
+        justify-content: space-between;
+
+        font-family: "KoreanKRSM", sans-serif;
+        background-color: red;
+        background-image: url("../../assets/p5cc-bg.png");
+        background-position: center;
+        padding: 50px 0 50px 0;
+
+        color: white;
+        text-shadow: 2px 2px black;
+        -webkit-text-stroke-width: 0.5px;
+        -webkit-text-stroke-color: black;
+
+        .content {
+            flex-direction: column;
+            justify-content: center;
+            text-align: center;
+            box-shadow: none;
+        }
+
+        p {
+            font-size: 1.5em;
+        }
+
+        img {
+            transform: rotateX(42deg) rotateY(-10deg) rotateZ(20deg);
+            filter: drop-shadow(2px 30px 10px rgb(174, 45, 45));
+            object-fit: contain;
+            padding-right: 5%;
+        }
+
+        button {
+            font-family: inherit;
+            font-size: inherit;
+
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            padding-left: 20px;
+            padding-right: 20px;
+
+            background-color: #fff;
+            border: 2px solid #000;
+            border-radius: 0;
+            box-shadow: 4px 4px #000;
+            color: #000;
+            padding: 10px;
+            outline: 0;
+
+            &:hover {
+                filter: invert(1);
+            }
+        }
+
+        @include for_breakpoint(mobile) {
+            flex-direction: column;
+            align-items: center;
+
+            img {
+                width: 75%;
+            }
+        }
+    }
+</style>
